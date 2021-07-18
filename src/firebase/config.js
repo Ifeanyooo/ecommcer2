d7ca45c9bd8450edcb85ed40d8fec65a116c98c5
Evimeromedia <evimeromedia@gmail.com>
2021-07-18 19:57:56 +0100

    adding files

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig =  {
+    apiKey: "AIzaSyCqRoHpOuK_-pIAh7sjc5O5BhXfV8dIFPQ",
+    authDomain: "ecommercewebsite-cd39a.firebaseapp.com",
+    projectId: "ecommercewebsite-cd39a",
+    storageBucket: "ecommercewebsite-cd39a.appspot.com",
+    messagingSenderId: "908251032916",
+    appId: "1:908251032916:web:fb9103fa476c02529c2cc1"
+  };
---
+++ src/pages/Registration/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Signup from './../../components/Signup'
+import './styles.scss';
+
+class Registration extends Component {
+    render() {
+        return <Signup/>;
+        
+    }
+
+}
+
+export default Registration;
+
+
